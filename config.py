625411da0d65e6d3399c4835385b97c1b5a5bf51
mfwolffe <wolffemf@dukes.jmu.edu>
2025-02-02 09:46:35 -0500

    template config

---
+++ config.py
@@ -0,0 +1,14 @@
+# config for the sftp client
+# this approach is impermanent, chap
+
+HOST    = "ASK.MATT.FOR.IT"
+PORT    = "^..."
+
+UNAME   = "DO YOU HAVE A USER CONFIGURED ON STARCHNET?"
+UPWD    = "^..."
+
+# switch to key based auth eventually
+KEYPATH = ""
+
+# not sure yet
+REMOTE_LANDING = ""
