292e614a49dac00d068b8b359872a204479ca4bf
mfwolffe <wolffemf@dukes.jmu.edu>
2025-02-03 13:10:17 -0500

    initial decorator for sanity's sake (and cleanliness)

---
+++ decorators.py
@@ -0,0 +1,14 @@
+# decorators for wulFTP routines
+
+import paramiko
+import socket
+
+def handle_excepts(fn):
+    def wrapper(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except Exception as e:
+            print(f"Fatal: Unexpected Error: {e}")
+        return False
+    return wrapper
+    
